9d9ff426f7cdcbc831654fa49d2852317092fc7d
Julie Coleman <juliecoleman820@gmail.com>
2020-05-07 19:42:48 +0000

    Part 1 completed

--- harvest.py
+++ harvest.py
@@ -6,18 +6,19 @@
 class MelonType(object):
     """A species of melon at a melon farm."""
 
-    def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
-                 name):
+    def __init__(self, code, name, first_harvest, color, is_seedless, 
+                 is_bestseller):
         """Initialize a melon."""
 
         self.pairings = []
 
         self.code = code
+        self.name = name
         self.first_harvest = first_harvest
         self.color = color
         self.is_seedless = is_seedless
         self.is_bestseller = is_bestseller
-        self.name = name
+        
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
@@ -35,19 +36,40 @@ def make_melon_types():
 
     all_melon_types = []
 
+    musk = MelonType('musk', 'Muskmelon', 1998, 'green', True, True)
+    musk.add_pairing('mint')
+    all_melon_types.append(musk)
 
+    cas = MelonType('cas', 'Casaba', 2003, 'orange', False, False)
+    cas.add_pairing('mint')
+    cas.add_pairing('strawberries')
+    all_melon_types.append(cas)
+
+    cren = MelonType('cren', 'Crenshaw', 1996, 'green', False, False)
+    cren.add_pairing('proscuitto')
+    all_melon_types.append(cren)
+
+    yw = MelonType('yw', 'Yellow Watermelon', 2013, 'yellow', False, True)
+    yw.add_pairing('ice cream')
+    all_melon_types.append(yw)
 
     return all_melon_types
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
-
-    # Fill in the rest
+    for melon in melon_types:
+        print(f"{melon.name} pairs with - ")
+        for pairing in melon.pairings:
+            print(f"{pairing}")
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
-    # Fill in the rest
+    for melon in melon_types:
+        melon_dictionary = {}
+        if melon.code not in melon_dictionary:
+            melon_dictionary[melon.code] = melon
+        return melon_dictionary
 
 ############
 # Part 2   #
